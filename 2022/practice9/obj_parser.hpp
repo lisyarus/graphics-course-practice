f0f15d510e379b659dc83f3324d2ec2579ff68b6
lisyarus <lisyarus@gmail.com>
2022-10-31 11:36:40 +0300

    Add practice 9

---
+++ 2022/practice9/obj_parser.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <array>
+#include <vector>
+#include <filesystem>
+
+struct obj_data
+{
+    struct vertex
+    {
+        std::array<float, 3> position;
+        std::array<float, 3> normal;
+        std::array<float, 2> texcoord;
+    };
+
+    std::vector<vertex> vertices;
+    std::vector<std::uint32_t> indices;
+};
+
+obj_data parse_obj(std::filesystem::path const & path);
