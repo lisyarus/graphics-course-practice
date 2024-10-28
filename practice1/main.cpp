53486905633591033c79b7afdf64d5e6b33c4f75
Aleksandr Belash <belash.aleksandr777@gmail.com>
2024-10-28 08:47:44 +0300

    Gradient triangle shaders

--- practice1/main.cpp
+++ practice1/main.cpp
@@ -11,6 +11,40 @@
 #include <stdexcept>
 #include <iostream>
 
+const char fragment_source[] =
+R"(#version 330 core
+
+layout (location = 0) out vec4 out_color;
+
+in vec3 color;
+void main()
+{
+    out_color = vec4(color, 1.0);
+}
+)";
+
+const char vertex_shader[] = 
+R"(#version 330 core
+
+const vec2 VERTICES[3] = vec2[3](
+vec2(0.0, 0.0),
+vec2(1.0, 0.0),
+vec2(0.0, 1.0)
+);
+
+out vec3 color;
+
+void main()
+{
+    gl_Position = vec4(VERTICES[gl_VertexID], 0.0, 1.0);
+    color = vec3(
+        gl_VertexID * (gl_VertexID - 2) * (-1.0),
+        gl_VertexID * (gl_VertexID - 1) * 0.5,
+        0.0
+    );
+}
+)";
+
 std::string to_string(std::string_view str)
 {
     return std::string(str.begin(), str.end());
@@ -24,6 +58,46 @@ void sdl2_fail(std::string_view message)
 void glew_fail(std::string_view message, GLenum error)
 {
     throw std::runtime_error(to_string(message) + reinterpret_cast<const char *>(glewGetErrorString(error)));
+}
+
+GLuint create_shader(GLenum shader_type, const char * shader_source) {
+    GLuint shader_id = glCreateShader(shader_type);
+    glShaderSource(shader_id, 1, &shader_source, NULL);
+    glCompileShader(shader_id);
+
+    GLint vertex_compiled;
+    glGetShaderiv(shader_id, GL_COMPILE_STATUS, &vertex_compiled);
+    if (vertex_compiled != GL_TRUE)
+    {
+        GLsizei log_length = 0;
+        GLchar message[1024];
+        glGetShaderInfoLog(shader_id, 1024, &log_length, message);
+        
+        throw std::runtime_error(to_string("Shader compile error: \"") + message + to_string("\""));
+    }
+
+    return shader_id;
+}
+
+GLuint create_program(GLuint vertex_shader, GLuint fragment_shader) {
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    GLint program_linked;
+    glGetProgramiv(program, GL_LINK_STATUS, &program_linked);
+
+    if (program_linked != GL_TRUE)
+    {
+        GLsizei log_length = 0;
+        GLchar message[1024];
+        glGetProgramInfoLog(program, 1024, &log_length, message);
+        
+        throw std::runtime_error(to_string("Program linking error: \"") + message + to_string("\""));
+    }
+
+    return program;
 }
 
 int main() try
@@ -57,6 +131,12 @@ int main() try
 
     glClearColor(0.8f, 0.8f, 1.f, 0.f);
 
+    GLuint shader1 = create_shader(GL_FRAGMENT_SHADER, fragment_source);
+    GLuint shader2 = create_shader(GL_VERTEX_SHADER, vertex_shader);
+    GLuint program = create_program(shader2, shader1);
+    GLuint vao[1];
+    glGenVertexArrays(1, vao);
+
     bool running = true;
     while (running)
     {
@@ -71,6 +151,10 @@ int main() try
             break;
 
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(program);
+        glBindVertexArray(vao[0]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         SDL_GL_SwapWindow(window);
     }
